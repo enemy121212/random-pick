ec5869e853378b404ac3dc505dd55aa62bd4609b
enemy121212 <earthling7800@gmail.com>
2026-05-10 11:05:28 +0300

    Create hero.js

---
+++ hero.js
@@ -0,0 +1,276 @@
+ const hero_list=[
+    {
+      name: 'Abaddon',
+      img: 'pub/heroes/abaddon.png',
+      role: '4'
+    },
+    {
+      name: 'Alchemist',
+      img: 'pub/heroes/alchemist.png',
+      role: '1'
+    },
+    {
+      name: 'Ancient Apparition',
+      img: 'pub/heroes/ancient_apparition.png',
+      role: '5'
+    },
+    {
+      name: 'Anti Mage',
+      img: 'pub/heroes/anti_mage.png',
+      role: '1'
+    },
+    {
+      name: 'Arc Warden',
+      img: 'pub/heroes/arc_warden.png',
+      role: '2'
+    },
+    {
+      name: 'Axe',
+      img: 'pub/heroes/axe.png',
+      role: '3'
+    },
+    {
+      name: 'Bane',
+      img: 'pub/heroes/bane.png',
+      role: '4'
+    },
+    {
+      name: 'Batrider',
+      img: 'pub/heroes/batrider.png',
+      role: '3'
+    },
+    {
+      name: 'Beastmaster',
+      img: 'pub/heroes/beastmaster.png',
+      role: '3'
+    },
+    {
+      name: 'Bloodseeker',
+      img: 'pub/heroes/bloodseeker.png',
+      role: '2'
+    },
+    {
+      name: 'Bounty Hunter',
+      img: 'pub/heroes/bounty_hunter.png',
+      role: '4'
+    },
+    {
+      name: 'Brewmaster',
+      img: 'pub/heroes/brewmaster.png',
+      role: '3'
+    },
+    {
+      name: 'Bristleback',
+      img: 'pub/heroes/bristleback.png',
+      role: '3'
+    },
+    {
+      name: 'Broodmother',
+      img: 'pub/heroes/broodmother.png',
+      role: '2'
+    },
+    {
+      name: 'Centaur Warrunner',
+      img: 'pub/heroes/centaur_warrunner.png',
+      role: '3'
+    },
+    {
+      name: 'Chaos Knight',
+      img: 'pub/heroes/chaos_knight.png',
+      role: '3'
+    },
+    {
+      name: 'Chen',
+      img: 'pub/heroes/chen.png',
+      role: '5'
+    },
+    {
+      name: 'Clinkz',
+      img: 'pub/heroes/clinkz.png',
+      role: '1'
+    },
+    {
+      name: 'Clockwerk',
+      img: 'pub/heroes/clockwerk.png',
+      role: '4'
+    },
+    {
+      name: 'Crystal Maiden',
+      img: 'pub/heroes/crystal_maiden.png',
+      role: '5'
+    },
+    {
+      name: 'Dark Seer',
+      img: 'pub/heroes/dark_seer.png',
+      role: '3'
+    },
+    {
+      name: 'Dark Willow',
+      img: 'pub/heroes/dark_willow.png',
+      role: '4'
+    },
+    {
+      name: 'Dawnbreaker',
+      img: 'pub/heroes/dawnbreaker.png',
+      role: '3'
+    },
+    {
+      name: 'Dazzle',
+      img: 'pub/heroes/dazzle.png',
+      role: '4'
+    },
+    {
+      name: 'Death Prophet',
+      img: 'pub/heroes/death_prophet.png',
+      role: '2'
+    },
+    {
+      name: 'Disruptor',
+      img: 'pub/heroes/disruptor.png',
+      role: '4'
+    },
+    {
+      name: 'Doom',
+      img: 'pub/heroes/doom.png',
+      role: '3'
+    },
+    {
+      name: 'Dragon Knight',
+      img: 'pub/heroes/dragon_knight.png',
+      role: '3'
+    },
+    {
+      name: 'Drow Ranger',
+      img: 'pub/heroes/drow_ranger.png',
+      role: '1'
+    },
+    {
+      name: 'Earth Spirit',
+      img: 'pub/heroes/earth_spirit.png',
+      role: '4'
+    },
+    {
+      name: 'Earthshaker',
+      img: 'pub/heroes/earthshaker.png',
+      role: '4'
+    },
+    {
+      name: 'Elder Titan',
+      img: 'pub/heroes/elder_titan.png',
+      role: '4'
+    },
+    {
+      name: 'Ember Spirit',
+      img: 'pub/heroes/ember_spirit.png',
+      role: '2'
+    },
+    {
+      name: 'Enchantress',
+      img: 'pub/heroes/enchantress.png',
+      role: '5'
+    },
+    {
+      name: 'Enigma',
+      img: 'pub/heroes/enigma.png',
+      role: '4'
+    },
+    {
+      name: 'Faceless Void',
+      img: 'pub/heroes/faceless_void.png',
+      role: '1'
+    },
+    {
+      name: 'Grimstroke',
+      img: 'pub/heroes/grimstroke.png',
+      role: '4'
+    },
+    {
+      name: 'Gyrocopter',
+      img: 'pub/heroes/gyrocopter.png',
+      role: '1'
+    },
+    {
+      name: 'Hoodwink',
+      img: 'pub/heroes/hoodwink.png',
+      role: '4'
+    },
+    {
+      name: 'Huskar',
+      img: 'pub/heroes/huskar.png',
+      role: '2'
+    },
+    {
+      name: 'Invoker',
+      img: 'pub/heroes/invoker.png',
+      role: '2'
+    },
+    {
+      name: 'Io',
+      img: 'pub/heroes/io.png',
+      role: '5'
+    },
+    {
+      name: 'Jakiro',
+      img: 'pub/heroes/jakiro.png',
+      role: '5'
+    },
+    {
+      name: 'Juggernaut',
+      img: 'pub/heroes/juggernaut.png',
+      role: '1'
+    },
+    {
+      name: 'Keeper of the Light',
+      img: 'pub/heroes/keeper_of_the_light.png',
+      role: '4'
+    },
+    {
+      name: 'Kunkka',
+      img: 'pub/heroes/kunkka.png',
+      role: '3'
+    },
+    {
+      name: 'Legion Commander',
+      img: 'pub/heroes/legion_commander.png',
+      role: '3'
+    },
+    {
+      name: 'Leshrac',
+      img: 'pub/heroes/leshrac.png',
+      role: '2'
+    },
+    {
+      name: 'Lich',
+      img: 'pub/heroes/lich.png',
+      role: '5'
+    },
+    {
+      name: 'Lifestealer',
+      img: 'pub/heroes/lifestealer.png',
+      role: '1'
+    },
+    {
+      name: 'Lina',
+      img: 'pub/heroes/lina.png',
+      role: '2'
+    },
+    {
+      name: 'Lion',
+      img: 'pub/heroes/lion.png',
+      role: '4'
+    },
+    {
+      name: 'Lone Druid',
+      img: 'pub/heroes/lone_druid.png',
+      role:'2'
+    },
+    {
+      name: 'Kez',
+      img: 'pub/heroes/kez.png',
+      roles:'1'
+    },
+    {
+      name: 'Largo',
+      img: 'pub/heroes/largo.png',
+    }
+    ]
