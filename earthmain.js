5e4191a908f1a975928bb6b9d1ce6ed955b01800
Earthlingreat <artas040609@gmail.com>
2026-05-24 17:32:14 +0300

    сделать ячейки кнопками

--- earthmain.js
+++ earthmain.js
@@ -1,3 +1,9 @@
 <script>
     console.log("Этот код работает внутри HTML");
 </script>
+
+document.querySelectorAll(".menu-btn").forEach(btn => {
+    btn.addEventListener("click", () => {
+        console.log("Нажата:", btn.textContent);
+    });
+});
